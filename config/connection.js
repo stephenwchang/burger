3684a8618ea0bb33c2a471bb414aee98a0f19495
Stephen Chang <stephenwchang@gmail.com>
2019-05-29 17:50:22 -0400

    a

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,22 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('heroku_304415b83a57f98', 'bd4584b97bad4f', '8f7d7963', {
-  host: 'localhostus-cdbr-iron-east-02.cleardb.net',
-  dialect: 'mysql',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+  // the application is executed on Heroku ... use the postgres database
+const sequelize = new Sequelize({
+    connectionString: process.env.DATABASE.URL,
+    ssl: true,
+  });
+
+  sequelize.connect();
+
+// const sequelize = new Sequelize('burgers_db', 'root', 'password', {
+//   host: 'localhost',
+//   port: 3306,
+//   dialect: 'mysql',
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   }
+// });
 
 module.exports = sequelize;
